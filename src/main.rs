b6c9b3101c0b86bdc582598821a8a2634b29783a
Dmitrii Demenev <demenev.dmitriy1@gmail.com>
2023-10-09 08:15:56 -0600

    feat: started implementing Compute for CopyingXor

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use rayon::prelude::*;
+use smallvec::SmallVec;
 
 const THRESHOLD: usize = 200_000;
 
+// This is a generic trait that is an overkill for many use cases.
+// Ideally, it should be implemented for simpler traits.
 pub trait Compute: Sized {
     type Output;
     type Subtasks;
@@ -27,6 +30,62 @@ pub trait Compute: Sized {
         } else {
             self.compute_multi_threaded()
         }
+    }
+}
+
+struct CopyingXor<'a>(&'a [u8]);
+
+impl<'a> Compute for CopyingXor<'a> {
+    type Output = Vec<u8>;
+    // TODO: find a way to make this constant better
+    type Subtasks = SmallVec<[CopyingXor<'a>; 32]>;
+    type Subtask = CopyingXor<'a>;
+    type ComputeSubtaskOutput = Vec<u8>;
+    type DistributeOutput = ();
+
+    fn complexity(&self) -> usize {
+        self.0.len()
+    }
+
+    fn compute_single_threaded(self) -> Self::Output {
+        let mut out = Vec::with_capacity(self.0.len());
+        out.extend(self.0.iter().map(|i| *i ^ 0xFF));
+        out
+    }
+
+    fn split(self) -> Self::Subtasks {
+        let Self(inp) = self;
+        let cpu_count = num_cpus::get();
+        let mut subtasks = SmallVec::with_capacity(cpu_count);
+        // the following assumes that the threads are of equal power
+        let smallest_chunk_size = inp.len() / cpu_count;
+        let remainer = inp.len() - smallest_chunk_size * cpu_count;
+        let subtask_iter = (0..cpu_count).map(|i| {
+            let start = i * smallest_chunk_size + std::cmp::min(i, remainer);
+            let end = start + smallest_chunk_size + if i < remainer { i } else { 0 };
+            let Some(slice) = inp.get(start..end) else {
+                unreachable!()
+            };
+            Self(slice)
+        });
+        subtasks.extend(subtask_iter);
+        subtasks
+    }
+
+    fn distribute(subtasks: Self::Subtasks) -> Self::DistributeOutput {
+        todo!()
+    }
+
+    fn compute_subtask(subtask: Self::Subtask) -> Self::ComputeSubtaskOutput {
+        todo!()
+    }
+
+    fn subtask_on_finish(subtask: Self::Subtask, subtask_output: Self::ComputeSubtaskOutput) {
+        todo!()
+    }
+
+    fn join(distributed_output: Self::DistributeOutput) -> Self::Output {
+        todo!()
     }
 }
 
