5d4031710c07494b5eacf608baec13048b097890
Dmitrii Demenev <demenev.dmitriy1@gmail.com>
2023-10-09 06:47:47 -0600

    feat: initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use rayon::prelude::*;
+
+const THRESHOLD: usize = 200_000;
+
+fn copying_xor(inp: &mut [u8]) -> Vec<u8> {
+    let mut out = Vec::with_capacity(inp.len());
+
+    let f = |i: &u8| *i ^ 0xFF;
+
+    if inp.len() < THRESHOLD {
+        out.extend(inp.iter().map(f))
+    } else {
+        out.par_extend(inp.par_iter().map(f));
+    }
+
+    out
+}
+
+fn main() {
+    let mut inp: Vec<u8> = (0..255).collect();
+    let v = copying_xor(&mut inp);
+    assert!(v.windows(2).all(|w| w[0] > w[1]));
+    println!("{v:?}");
+}
