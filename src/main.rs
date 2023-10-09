3b547e522002a5c7170dc5ad49dd5e33a10b7212
Dmitrii Demenev <demenev.dmitriy1@gmail.com>
2023-10-09 07:00:01 -0600

    fix: eased the requirements for the caller

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use rayon::prelude::*;
 
 const THRESHOLD: usize = 200_000;
 
-fn copying_xor(inp: &mut [u8]) -> Vec<u8> {
+fn copying_xor(inp: &[u8]) -> Vec<u8> {
     let mut out = Vec::with_capacity(inp.len());
 
     let f = |i: &u8| *i ^ 0xFF;
@@ -17,8 +17,8 @@ fn copying_xor(inp: &mut [u8]) -> Vec<u8> {
 }
 
 fn main() {
-    let mut inp: Vec<u8> = (0..255).collect();
-    let v = copying_xor(&mut inp);
+    let inp: Vec<u8> = (0..255).collect();
+    let v = copying_xor(&inp);
     assert!(v.windows(2).all(|w| w[0] > w[1]));
     println!("{v:?}");
 }
